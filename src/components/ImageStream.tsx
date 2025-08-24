24db9c47fe97ffe4b97ba71b704ff83cb986774c
Zelch Csaba <zch03po@gmail.com>
2025-08-24 18:06:47 +0300

    camera shoot disallowed

--- src/components/ImageStream.tsx
+++ src/components/ImageStream.tsx
@@ -1,17 +1,17 @@
 import { useWS } from '../WsContext';
 
 export default function ImageStream() {
-  const { sendMessage } = useWS();
-  const handleClick = () => {
-    sendMessage({ type: "button", value: "click" });
-  };
+  //const { sendMessage } = useWS();
+  //const handleClick = () => {
+  //  sendMessage({ type: "button", value: "click" });
+  //};
 
   const { imageData } = useWS();
   return (
     <div className="fixed left-1/2 transform -translate-x-1/2 w-1/2 h-13/15 flex items-center justify-center bg-gray-800 select-none">
   {imageData ? (
     <button
-      onClick={handleClick}
+      //onClick={handleClick}
       className="w-full h-full rounded-2xl border-4 border-gray-600 bg-black p-0 m-0 cursor-pointer
              active:scale-95 active:brightness-90 transition-transform duration-150"
     >
--- src/components/ImageStreamUp.tsx
+++ src/components/ImageStreamUp.tsx
@@ -2,16 +2,16 @@ import { useWS } from '../WsContext';
 
 export default function ImageStream() {
   const { sendMessage } = useWS();
-  const handleClick = () => {
-    sendMessage({ type: "button", value: "click" });
-  };
+  //const handleClick = () => {
+  //  sendMessage({ type: "button", value: "click" });
+  //};
   const { imageData } = useWS();
 
   return (
     <div className="w-screen h-7/15 fixed top-0 left-0 flex items-center justify-center bg-gray-800 select-none">
   {imageData ? (
     <button
-      onClick={handleClick}
+      //onClick={handleClick}
       className="w-full h-full rounded-2xl border-4 border-gray-600 bg-black p-0 m-0 cursor-pointer
                  active:scale-95 active:brightness-90 transition-transform duration-150"
     >
